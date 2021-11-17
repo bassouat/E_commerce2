a3671e6e7c109f29d73bde18837abbf9f22a5f03
Basseydou Ouattara <bassouat8@gmail.com>
2021-11-17 23:02:07 +0100

    Vas-y tu y arriveras

---
+++ src/main/java/com/educative/ecommerce/service/ProductService.java
@@ -0,0 +1,11 @@
+package com.educative.ecommerce.service;
+
+import com.educative.ecommerce.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+}
